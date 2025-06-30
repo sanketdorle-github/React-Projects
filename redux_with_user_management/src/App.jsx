d61d1ddda85779a5f85e05c06b3b9ac334cc1c79
sanketLocal <sanketgdorle200105@gmial.com>
2025-07-01 00:30:16 +0530

    adding few changes only

--- redux_with_user_management/src/App.jsx
+++ redux_with_user_management/src/App.jsx
@@ -1,7 +1,34 @@
 import "./App.css";
 
+import {Route, Router, Routes,createBrowserRouter,BrowserRouter} from "react-router-dom"
+import HomePage from "./pages/HomePage.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+
 const App = () => {
-  return <div>App</div>;
+  let navLinks=[{
+    pathTo:"/",compTo:<HomePage/>
+  },
+{
+    pathTo:"/login",compTo:<Login/>
+  },
+{
+    pathTo:"/register",compTo:<Register/>
+  }]
+  return <>
+  
+  <BrowserRouter>
+    <Routes>
+      {
+        navLinks.map((navLink)=> (
+          <>
+          <Route path={navLink.pathTo}  element={navLink.compTo}/>
+          </>
+        ))
+      }
+    </Routes>
+  </BrowserRouter>
+  </>
 };
 
 export default App;
