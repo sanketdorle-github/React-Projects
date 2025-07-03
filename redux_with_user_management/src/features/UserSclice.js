b48290cea5b20ed565db471df572d08b5594f3cd
sanketLocal <sanketgdorle200105@gmial.com>
2025-07-03 23:13:51 +0530

    added few changes

--- redux_with_user_management/src/features/UserSclice.js
+++ redux_with_user_management/src/features/UserSclice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  user: null,
+  user: {},
+  token: null,
   isAuthenticated: false,
   error: null,
   loading: false,
@@ -12,12 +13,19 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     loginSuccess(state, action) {
-      state.user = action.payload;
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+
       state.isAuthenticated = true;
+            localStorage.setItem('user', JSON.stringify(action.payload.user)); // Only non-sensitive data
+            localStorage.setItem('token', action.payload.token); // Only non-sensitive data
     },
     logout(state) {
       state.user = null;
       state.isAuthenticated = false;
+       localStorage.removeItem('user');
+       localStorage.removeItem('token');
+      // Server should also invalidate the refresh token
     },
   },
 });
--- redux_with_user_management/src/pages/Login.jsx
+++ redux_with_user_management/src/pages/Login.jsx
@@ -1,6 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styling/Login.module.css";
+import { toast } from "react-toastify";
+
+
+import {useDispatch,useSelector} from "react-redux";
+import { loginSuccess } from "../features/UserSclice";
+
+
+
+
 const Login = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state)=>state.auth.user);
+  const token = useSelector((state)=>state.auth.token);
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [isLoading, setisLoading] = useState(false);
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const userData = {
+    email: formData.email,
+    password: formData.password,
+  };
+  try {
+    setisLoading(true);
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/users/login`, {
+      method: "POST",
+      credentials: "include", // Required for cookies
+      headers: {
+        "Content-Type": "application/json",
+        
+      },
+      body: JSON.stringify(userData),
+    });
+    const responseData = await response.json();
+    if (response.ok) {
+      // Handle successful login
+      console.log("Login successful:", responseData);
+      const data = responseData.data
+      dispatch(loginSuccess(data));
+      toast.success("Login successful!");
+    } else {
+      // Handle login error
+      console.error("Login failed:", responseData);
+      toast.error(responseData.message || "Login failed. Please try again.");
+    }
+  } catch (error) {
+    console.error("Error logging in:", error);
+  } finally {
+    setisLoading(false);
+  }
+}
   return (
     <div className={styles.container}>
       <h3>Login User</h3>
@@ -13,15 +66,23 @@ const Login = () => {
               name="email"
               id="email"
               placeholder="jdoe@example.com"
+              value={formData.email}
+              onChange={(e) =>setFormData({...formData, email:e.target.value})}
             />
           </div>
 
           <div>
             <label htmlFor="email">Password:</label>
-            <input type="password" name="password" id="password" />
+            <input type="password" name="password" id="password" 
+              value={formData.password}
+              onChange={(e) =>setFormData({...formData, password:e.target.value})}
+            />
           </div>
-          <input type="submit" value="Submit" className="registerBtn" />
+          <input type="submit" value="Submit" className="registerBtn"  onClick={handleSubmit} disabled={isLoading}/>
         </form>
+        <p>{user && user.email} </p>
+        <p>{user && user.firstName} </p>
+      
       </div>
     </div>
   );
